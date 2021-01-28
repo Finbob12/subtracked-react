e9b15a239109e3a8502b689fb2653e271f1d0d92
Fin <finlaytgeorge@gmail.com>
2021-01-28 12:55:07 +1100

    small changes to nav

--- src/NavbarPage.js
+++ src/NavbarPage.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
-import Form from 'react-bootstrap/Form'
-import FormControl from 'react-bootstrap/FormControl'
-import Button from 'react-bootstrap/Button'
+
+// import Form from 'react-bootstrap/Form'
+// import FormControl from 'react-bootstrap/FormControl'
+// import Button from 'react-bootstrap/Button'
 
 // state = {
 //   isOpen: false
@@ -16,19 +17,19 @@ import Button from 'react-bootstrap/Button'
 const NavbarPage = () => {
   return (
     <div>
-      <Navbar bg="dark" variant="dark">
-      <Navbar.Brand href="#home">SubTracked</Navbar.Brand>
-      <Nav className="mr-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#my-subs">My Subs</Nav.Link>
-        <Nav.Link href="#new-sub">New Sub</Nav.Link>
-        <Nav.Link href="#account">Account</Nav.Link>
-        <Nav.Link href="#log">Log In/Out</Nav.Link>
-      </Nav>
-      <Form inline>
-        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-        <Button variant="outline-info">Search</Button>
-      </Form>
+      <Navbar className="navbar-main" variant="dark " sticky="top">
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#my-subs">My Subs</Nav.Link>
+          <Nav.Link href="#new-sub">New Sub</Nav.Link>
+          <Nav.Link href="#account">Account</Nav.Link>
+        </Nav>
+        <Navbar.Brand href="#home" className="navbar-brand mx-auto">
+          SubTracked
+        </Navbar.Brand>
+        <Nav className="ml-auto">
+          <Nav.Link href="#log">Log In/Out</Nav.Link>
+        </Nav>
       </Navbar>
     </div>
   )

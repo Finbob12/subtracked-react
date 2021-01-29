eb323c0719925d00fbbae4aa1d887829e144d40f
Fin <finlaytgeorge@gmail.com>
2021-01-29 11:57:51 +1100

    static forms set up

--- src/LogIn.js
+++ src/LogIn.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
 const LogIn = () => {
     return (
-        <div>
-            Log In
+        <div className="container mt-3">
+            <div className="row">
+                <div className="col-md-5 d-flex align-items-center">
+                    <h1 className="form-heading">Log In</h1>
+                </div>
+                <Form className="col-md-6 offset-md-1">
+                    <Form.Group controlId="li-email">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Text className="text-muted">
+                            We'll never share your email with anyone else.
+                        </Form.Text>
+                    </Form.Group>
+                    <Form.Group controlId="li-username">
+                        <Form.Label>Username</Form.Label>
+                        <Form.Control type="text" placeholder="Username" />
+                    </Form.Group>
+                    <Form.Group controlId="li-password">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+            </div>
         </div>
     )
 }
--- src/NewSub.js
+++ src/NewSub.js
@@ -1,10 +1,47 @@
 import React from 'react'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
 const NewSub = () => {
     return (
-        <div>
-            New Sub
+    <div className="container mt-3">
+        <div className="row">
+        <div className="col-md-5 d-flex align-items-center">
+            <h1 className="form-heading">Add a new subscription!</h1>
         </div>
+            <Form className="col-md-6 offset-md-1">
+                <Form.Group controlId="name">
+                    <Form.Label>Subscription Name</Form.Label>
+                    <Form.Control type="text" placeholder="Search as user types" />
+                </Form.Group>
+                <Form.Group controlId="description">
+                    <Form.Label>Description</Form.Label>
+                    <Form.Control as="textarea" rows={3} placeholder="Description just for you to keep track of what this subscription is" />
+                </Form.Group>
+                <Form.Group controlId="cycle">
+                    <Form.Label>Cycle</Form.Label>
+                    <Form.Control as="select">
+                        <option>Weekly</option>
+                        <option>Fortnightly</option>
+                        <option>Month</option>
+                        <option>Quarterly</option>
+                        <option>Yearly</option>
+                    </Form.Control>
+                </Form.Group>
+                <Form.Group controlId="first-payment">
+                    <Form.Label>First Payment</Form.Label>
+                    <Form.Control type="date" />
+                </Form.Group>
+                <Form.Group controlId="last-payment">
+                    <Form.Label>Last Payment</Form.Label>
+                    <Form.Control type="date" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                        Submit
+                </Button>
+            </Form>
+        </div>
+    </div>
     )
 }
 

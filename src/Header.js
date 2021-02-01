5f1908e3bfb0ff984c9bafb92bff9d59ba5d2986
Fin <finlaytgeorge@gmail.com>
2021-02-01 12:33:46 +1100

    fix location of route rendering to app instead of index

--- src/Header.js
+++ src/Header.js
@@ -4,18 +4,6 @@ import Nav from 'react-bootstrap/Nav'
 import {LinkContainer} from 'react-router-bootstrap'
 
 
-// import Form from 'react-bootstrap/Form'
-// import FormControl from 'react-bootstrap/FormControl'
-// import Button from 'react-bootstrap/Button'
-
-// state = {
-//   isOpen: false
-// };
-
-// toggleCollapse = () => {
-//   this.setState({ isOpen: !this.state.isOpen });
-// }
-
 const Header = () => {
   return (
       <div>

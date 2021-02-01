474f8716a09d463cd8ee1ceab1f1549a843d1d33
Fin <finlaytgeorge@gmail.com>
2021-02-01 16:00:07 +1100

    fix input validation state

--- src/LogIn.js
+++ src/LogIn.js
@@ -71,18 +71,36 @@ class LogIn extends Component {
                     <Form onSubmit={this.handleSubmit} className="col-md-6 offset-md-1">
                         <Form.Group controlId="li-email">
                             <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" value={email} onChange={this.handleChange} />
-                            <Form.Text className="text-muted">
+                            <Form.Control
+                                placeholder="email"
+                                type="email"
+                                name="email"
+                                value={email}
+                                onChange={this.handleChange}
+                             />
+                             <Form.Text className="text-muted">
                                 We'll never share your email with anyone else.
                             </Form.Text>
                         </Form.Group>
                         <Form.Group controlId="li-first-name">
                             <Form.Label>First Name</Form.Label>
-                            <Form.Control type="text" placeholder="First Name" value={first_name} onChange={this.handleChange}  />
+                            <Form.Control
+                                placeholder="first name"
+                                type="text"
+                                name="first_name"
+                                value={first_name}
+                                onChange={this.handleChange}
+                             />
                         </Form.Group>
                         <Form.Group controlId="li-password">
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" value={password} onChange={this.handleChange}  />
+                            <Form.Control
+                                placeholder="password"
+                                type="password"
+                                name="password"
+                                value={password}
+                                onChange={this.handleChange}
+                             />
                         </Form.Group>
                         <Button variant="primary" type="submit">
                             Submit

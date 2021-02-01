9a9f3e3b201815d9138d1772f8875c11aa9d317b
Fin <finlaytgeorge@gmail.com>
2021-02-01 15:46:27 +1100

    initial state set for log in and set up, needs onchange fixes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './Header';
 import Account from './Account';
 import MySubs from './MySubs';
@@ -6,22 +6,64 @@ import NewSub from './NewSub';
 import LogIn from './LogIn';
 import SignUp from './SignUp';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import axios from 'axios'
 
 import './App.css';
 
-const App = () => (
-  <Router>
-  <Header />
- <Switch>
-   {/* <Route exact path="/" component={App} /> */}
-   <Route exact path="/new-sub" component={NewSub} />
-   <Route exact path="/account" component={Account} />
-   <Route exact path="/my-subs" component={MySubs} />
-   <Route exact path="/log-in" component={LogIn} />
-   <Route exact path="/sign-up" component={SignUp} />
- </Switch>
-</Router>
-);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      isLoggedIn: false,
+      user: {}
+     };
+  }
+componentDidMount() {
+  this.loginStatus()
+}
+
+loginStatus = () => {
+  axios.get('http://localhost:3000/logged_in', 
+  {withCredentials: true})    
+.then(response => {
+    if (response.data.logged_in) {
+      this.handleLogin(response)
+    } else {
+      this.handleLogout()
+    }
+  })
+  .catch(error => console.log('api errors:', error))
+}
+handleLogin = (data) => {
+  this.setState({
+    isLoggedIn: true,
+    user: data.user
+  })
+}
+handleLogout = () => {
+  this.setState({
+  isLoggedIn: false,
+  user: {}
+  })
+}
+
+render() {
+    return (
+      <div>
+         <BrowserRouter>
+         <Header />
+          <Switch>
+          <Route exact path="/new-sub" component={NewSub} />
+          <Route exact path="/account" component={Account} />
+          <Route exact path="/my-subs" component={MySubs} />
+          <Route exact path="/log-in" component={LogIn} />
+          <Route exact path="/sign-up" component={SignUp} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
+};
 
 export default App;
--- src/SignUp.js
+++ src/SignUp.js
@@ -1,42 +1,103 @@
-import React from 'react'
+import React, { Component } from 'react';
+import axios from 'axios'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 
+class SignUp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      first_name: '',
+      email: '',
+      password: '',
+      password_confirmation: '',
+      errors: ''
+     };
+  }
 
-const SignUp = () => {
-    return (
-        <div className="container mt-3">
-            <div className="row">
-                <div className="col-md-5 d-flex align-items-center">
-                    <h1 className="form-heading">Sign Up</h1>
-                </div>
-                <Form className="col-md-6 offset-md-1">
-                    <Form.Group controlId="si-email">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
-                        <Form.Text className="text-muted">
-                            We'll never share your email with anyone else.
-                        </Form.Text>
-                    </Form.Group>
-                    <Form.Group controlId="si-username">
-                        <Form.Label>Username</Form.Label>
-                        <Form.Control type="text" placeholder="Username" />
-                    </Form.Group>
-                    <Form.Group controlId="si-password">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
-                    <Form.Group controlId="si-confirm-password">
-                        <Form.Label>Confirm Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
-                    <Button variant="primary" type="submit">
-                        Submit
-                    </Button>
-                </Form>
+  handleChange = (event) => {
+    const {name, value} = event.target
+    this.setState({
+      [name]: value
+    })
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    const {first_name, email, password, password_confirmation} = this.state
+    let user = {
+        first_name: first_name,
+        email: email,
+        password: password,
+        password_confirmation: password_confirmation
+    }
+
+    axios.post('http://localhost:3000/users', {user}, {withCredentials: true})
+        .then(response => {
+            if (response.data.status === 'created') {
+                this.props.handleLogin(response.data)
+                this.redirect()
+            } else {
+                this.setState({
+                errors: response.data.errors
+                })
+            }
+        })
+        .catch(error => console.log('api errors:', error))
+    };
+
+    redirect = () => {
+        this.props.history.push('/')
+    }
+
+    handleErrors = () => {
+        return (
+            <div>
+                <ul>
+                {this.state.errors.map(error => {
+                    return <li key={error}>{error}</li>
+                    })}
+                </ul>
             </div>
-        </div>
-    )
+        )
+    };
+
+    render() {
+        const {first_name, email, password, password_confirmation} = this.state
+        return (
+            <div className="container mt-3">
+                <div className="row">
+                    <div className="col-md-5 d-flex align-items-center">
+                        <h1 className="form-heading">Sign Up</h1>
+                    </div>
+                    <Form onSubmit={this.handleSubmit} className="col-md-6 offset-md-1">
+                        <Form.Group controlId="si-email">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" value={email} onChange={this.handleChange} />
+                            <Form.Text className="text-muted">
+                                We'll never share your email with anyone else.
+                            </Form.Text>
+                        </Form.Group>
+                        <Form.Group controlId="si-first_name">
+                            <Form.Label>First Name</Form.Label>
+                            <Form.Control type="text" placeholder="First Name" value={first_name} onChange={this.handleChange} />
+                        </Form.Group>
+                        <Form.Group controlId="si-password">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" value={password} onChange={this.handleChange} />
+                        </Form.Group>
+                        <Form.Group controlId="si-confirm-password">
+                            <Form.Label>Confirm Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" value={password_confirmation} onChange={this.handleChange} />
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </Form>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default SignUp
